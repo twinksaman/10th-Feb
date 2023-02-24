368758515ef9694153fbb38338d9bfcbee83c189
twinksaman <125017247+twinksaman@users.noreply.github.com>
2023-02-24 17:59:50 +0530

    Update helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -1,7 +1,7 @@
 class Simple{
 
           public static void main(String args[]){
-          System.out.println("Hello Students from Omar Batc");
+          System.out.println("Hello Students from Omar Batch28 Aman");
 }
 
 }
